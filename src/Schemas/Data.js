f49276c342c7bcaf9741f97a5cd63eee83569eb1
Damon42Dev <damonwang42@gmail.com>
2025-08-28 22:44:21 +1000

    Initial commit: React resume project

---
+++ src/Schemas/Data.js
@@ -0,0 +1,117 @@
+export const Data = {
+  profile: {
+    name: "Damon Wang",
+    ocupation: "FullStack Software Developer",
+    location: "Melbourne, Australia",
+    email: "damonwang42@gmail.com",
+    telephone: "+61 xxxxxxxx",
+    image: "images/me.jpg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "FullStack Software Developer, with a passion for building scalable and efficient web applications. I am a quick learner and I am always seeking for new challenges and opportunities to grow.",
+  },
+  skills: {
+    technicalLabel: "Technologies",
+    softLabel: "Skills",
+    technicalSkills: [
+      "React",
+      "React Native",
+      "Nodejs",
+      "Java",
+      "Spring Boot",
+      "Nestjs",
+      "SQL",
+      "Git",
+      "Docker",
+      "Kubernetes",
+      "AWS",
+      "Azure",
+      "GCP",
+      "GitLab",
+      "CI/CD",
+      "Agile",
+      "Scrum",
+      "Kanban",
+      "Jira",
+      "Confluence",
+    ],
+    softSkills: [
+      "Problem Solving",
+      "Teamwork",
+      "Communication",
+      "Proactivity",
+      "Creativity",
+      "Autodidactism",
+      "Mentoring",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visit my LinkedIn profile",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/damonwang",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visit my GitHub profile",
+        name: "github",
+        url: "https://github.com/damonwang",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Software Engineer (FullStack)",
+        period: "Feb. 2021 - Current",
+        company: "ECAL Marketing Pty Ltd.",
+        description: [
+          "Development and continuous improvement of REST microservices in Java and Node.js(JS/TS).",
+          "Development and continuous improvement of PWA in the technologies Angular, Ionic and Cordova.",
+          "CI/CD pipeline with Bitbucket, Jenkins and AWS as infrastructure.",
+        ],
+      },
+      {
+        title: "IT Support",
+        period: "May. 2021 - Jun. 2021",
+        company: "Monash University",
+        description: [
+          "Delivered technical support to students and invigilators, ensuring a smooth and seamless online examination experience."
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Master of Business Information Systems",
+        date: "2019 - 2020 Melbourne, Australia",
+        institution: "Monash University",
+        award: "Awarded ‘Information Technology International Merit Scholarship’.",
+      }
+    ],
+    projects: [
+      {
+        name: "Onboarding Digital",
+        company: "Banco Ripley",
+        period: "Nov. 2021 - Ene. 2022",
+        description: [
+          "Development of REST Web Services in Java and Nodejs(JS/TS) accompanied by different database engines like Mysql, Oracle, MariaDB, MongoDB and IBM API Connect and 3scale as API Gateway.",
+          "Development of Frontend and Mobile with technologies like Angular, React, React Native, Ionic and Cordova.",
+        ],
+      },
+      {
+        name: "Digital Capture",
+        company: "Banco Ripley",
+        period: "Ene. 2022 - Feb. 2022",
+        description: [
+          "Control, desarrollo y securitización de APIs en plataforma IBM API Connect.",
+          "Desarrollo frontend en Angular para el consumo de Web Services SOAP/REST apuntando a ambientes DEV/PRE-PROD/PROD como simulación.",
+        ],
+      },
+    ],
+  },
+};
